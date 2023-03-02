bb52a5c060bf10b44f8ca006514382096539c109
avawaters <awaters@vassar.edu>
2023-03-02 11:56:35 -0500

    keep base64 data

--- melodic_generation.js
+++ melodic_generation.js
@@ -293,8 +293,6 @@ var trial_response = {
         // EDIT FOR EACH GENERATION
         const filename = `gen_0-${subject_id}-${data.melody}.webm`;
         jsPsychPipe.saveBase64Data("QfKXr6jPLyzT", filename, data.response);
-        // delete the base64 data to save space. store the filename instead.
-        data.response = null;
     }
 };
 
