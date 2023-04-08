04af4e465e0fb81cfa36be59fd5865df2f40c620
avawaters <awaters@vassar.edu>
2023-04-08 09:42:11 -0400

    set up gen_3

--- melodic_generation.js
+++ melodic_generation.js
@@ -9,23 +9,42 @@ var pitch_matching_stimuli;
 var practice_stimulus;
 var trial_stimuli;
 
+// alternate condition assignment
+var version;
 
-// arrays holding information for low stimuli
+async function createExperiment(){
+    const condition = await jsPsychPipe.getCondition("QfKXr6jPLyzT");
+    if(condition == 0) { version = 0; }
+    if (condition == 1) { version = 1; }
+    console.log(version);
+    jsPsych.run(timeline);
+  }
+  createExperiment();
+
+// arrays holding files for low stimuli
 var low_pitch_matching = ["stimuli/pitch_matching/pitch_matching-A2.mp3", "stimuli/pitch_matching/pitch_matching-D3.mp3", "stimuli/pitch_matching/pitch_matching-G3.mp3"];
 //EDIT EACH GEN
-var low_stimuli = ["stimuli/low/seed_1p-gen_2-low.mp3", "stimuli/low/seed_2p-gen_2-low.mp3", "stimuli/low/seed_3p-gen_2-low.mp3", "stimuli/low/seed_4p-gen_2-low.mp3"];
+var low_stimuli = [["stimuli/low/seed_1p-gen_3-low.mp3", "stimuli/low/seed_2p-gen_3-low.mp3",
+                    "stimuli/low/seed_3u-gen_3-low.mp3", "stimuli/low/seed_4u-gen_3-low.mp3"],
+                    ["stimuli/low/seed_1u-gen_3-low.mp3", "stimuli/low/seed_2u-gen_3-low.mp3",
+                    "stimuli/low/seed_3p-gen_3-low.mp3", "stimuli/low/seed_4p-gen_3-low.mp3"]];
 
 
-// arrays holding information for high stimuli
+
+// arrays holding files for high stimuli
 var high_pitch_matching = ["stimuli/pitch_matching/pitch_matching-A3.mp3", "stimuli/pitch_matching/pitch_matching-D4.mp3", "stimuli/pitch_matching/pitch_matching-G4.mp3"];
 //EDIT EACH GEN
-var high_stimuli = ["stimuli/high/seed_1p-gen_2-high.mp3", "stimuli/high/seed_2p-gen_2-high.mp3", "stimuli/high/seed_3p-gen_2-high.mp3", "stimuli/high/seed_4p-gen_2-high.mp3"];
+var high_stimuli = [["stimuli/high/seed_1p-gen_3-high.mp3", "stimuli/high/seed_2p-gen_3-high.mp3",
+                    "stimuli/high/seed_3u-gen_3-high.mp3", "stimuli/high/seed_4u-gen_3-high.mp3"],
+                    ["stimuli/high/seed_1u-gen_3-high.mp3", "stimuli/high/seed_2u-gen_3-high.mp3",
+                    "stimuli/high/seed_3p-gen_3-high.mp3", "stimuli/high/seed_4p-gen_3-high.mp3"]];
+
 
 
 // capture info from Prolific
 const subject_id = jsPsych.data.getURLVariable('PROLIFIC_PID');
 //EDIT EACH GEN
-const fname = `gen_2-${subject_id}.json`;
+const fname = `gen_3-${subject_id}.json`;
 
 /**************************************** EXPERIMENT EVENTS ****************************************/
 var preload = {
@@ -119,8 +138,8 @@ var range_q = {
                 name: ["practice"]
             };
             trial_stimuli = {
-                file: low_stimuli,
-                name: low_stimuli.map(get_melody)
+                file: low_stimuli[version],
+                name: low_stimuli[version].map(get_melody)
             }
         }
         // high stimuli
@@ -134,8 +153,8 @@ var range_q = {
                 name: ["practice"]
             };
             trial_stimuli = {
-                file: high_stimuli,
-                name: high_stimuli.map(get_melody)
+                file: high_stimuli[version],
+                name: high_stimuli[version].map(get_melody)
             }
         }
     }
@@ -169,7 +188,7 @@ var pitch_matching_response = {
     on_finish: function (data) {
         // filename example: gen0-1234-A2.webm
         //EDIT EACH GEN
-        const filename = `gen_2-${subject_id}-${data.pitch}.webm`;
+        const filename = `gen_3-${subject_id}-${data.pitch}.webm`;
         jsPsychPipe.saveBase64Data("QfKXr6jPLyzT", filename, data.response);
     }
 };
@@ -290,7 +309,7 @@ var trial_response = {
     on_finish: function (data) {
         // filename example: gen0-1234-seed_1.webm
         //EDIT EACH GEN
-        const filename = `gen_2-${subject_id}-${data.melody}.webm`;
+        const filename = `gen_3-${subject_id}-${data.melody}.webm`;
         jsPsychPipe.saveBase64Data("QfKXr6jPLyzT", filename, data.response);
     }
 };
@@ -423,4 +442,3 @@ var conditional_full_debrief = {
 
 timeline.push(debrief, conditional_full_debrief);
 
-jsPsych.run(timeline);
